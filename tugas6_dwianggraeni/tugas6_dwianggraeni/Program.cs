6fb35bae1841fe1427c44aa190282658dba6ef24
anggraeni258712-coder <anggraeni258712@student.smkn1kandeman.sch.id>
2025-11-24 14:48:56 +0700

    Add files via upload

---
+++ tugas6_dwianggraeni/tugas6_dwianggraeni/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas6_dwianggraeni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            { //Membuat array untuk menyimpan nama, nilai, dan jenis kelamin 3 siswa
+                string[] nama = new string[3];
+                char[] jk = new char[3];
+                string[] kelas = new string[3];
+
+                //Input nama dan nilai siswa dari keyboard
+                Console.Write("Masukkan nama siswa ke-1: ");
+                nama[0] = Console.ReadLine();
+                Console.Write("Masukkan jenis kelamin siswa ke-1 (L/P): ");
+                jk[0] = Convert.ToChar(Console.ReadLine());
+                Console.Write("Masukkan kelas siswa ke-1: ");
+                kelas[0] = Console.ReadLine();
+
+                Console.Write("Masukkan nama siswa ke-2: ");
+                nama[1] = Console.ReadLine();
+                Console.Write("Masukkan jenis kelamin siswa ke-2 (L/P): ");
+                jk[1] = Convert.ToChar(Console.ReadLine());
+                Console.Write("Masukkan kelas siswa ke-2: ");
+                kelas[1] = Console.ReadLine();
+
+                Console.Write("Masukkan nama siswa ke-3: ");
+                nama[2] = Console.ReadLine();
+                Console.Write("Masukkan jenis kelamin siswa ke-3 (L/P): ");
+                jk[2] = Convert.ToChar(Console.ReadLine());
+                Console.Write("Masukkan kelas siswa ke-3: ");
+                kelas[2] = Console.ReadLine();
+
+
+
+                //Menampilkan hasil input tanpa perulangan 
+                Console.WriteLine("\n=== Data Siswa ===");
+                Console.WriteLine("Nama: " + nama[0] + " | Jenis Kelamin: " + jk[0] + " | Kelas: " + kelas[0]);
+                Console.WriteLine("Nama: " + nama[1] + " | Jenis Kelamin: " + jk[1] + " | Kelas: " + kelas[1]);
+                Console.WriteLine("Nama: " + nama[2] + " | Jenis Kelamin: " + jk[2] + " | Kelas: " + kelas[2]);
+            }
+        }
+    }
+}
---
+++ tugas7.3_dwianggraeni/tugas7.3_dwianggraeni/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas7._3_dwianggraeni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
