c787914634c668b0a0db635b408930373a1a31df
anggraeni258712-coder <anggraeni258712@student.smkn1kandeman.sch.id>
2025-11-24 14:59:00 +0700

    Add files via upload

---
+++ praktik_5.1_dwianggraeni_xpplg1/praktik_5.1_dwianggraeni_xpplg1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_5._1_dwianggraeni_xpplg1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("perulangan untuk menampilkan angka 1 sampai 10:");
+
+            // Perulangan for di mulai di sini
+            for (int i = 1; i <= 10; i++)
+            {
+                // Blok kode yang akan diulang
+                Console.WriteLine("angka ke-" + i);
+            }
+
+            Console.WriteLine("\nPerulangan selesai.");
+        }
+    }
+}
---
+++ praktik_5.8_dwianggraeni_xpplg1/praktik_5.8_dwianggraeni_xpplg1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_5._8_dwianggraeni_xpplg1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ulangiPilihan;
+
+            do
+            {
+                // Blok Proses yang Akan Diulang:
+                Console.WriteLine("================================");
+                Console.Write("Masukkan nama Anda: ");
+                string nama = Console.ReadLine();
+                Console.WriteLine($"Halo, {nama}! Proses telah selesai.");
+
+                // Pertanyaan untuk mengulang
+                Console.Write("Apakah Anda Ingin mengulang lagi? (ya/tidak): ");
+                // Mengambil input dan mengubah ke huruf kecil
+                ulangiPilihan = Console.ReadLine().ToLower();
+
+                Console.WriteLine(); // Baris kosong untuk kerapatan
+
+                // Kondisi diperiksa di akhir. Ulangi selama pengguna mengetik "ya"
+            } while (ulangiPilihan == "ya");
+
+            Console.WriteLine("Terima kasih. Program selesai.");
+            
+        }
+    }
+}
